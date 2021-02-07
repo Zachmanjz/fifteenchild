559bf50937766628b9def9c044934b647cc7266a
Jeremy Zach <44850631+Zachmanjz@users.noreply.github.com>
2021-02-06 21:58:34 -0500

    Updated functions.php
    Show what template file is being used in WordPress

--- functions.php
+++ functions.php
@@ -6,3 +6,12 @@ function fifteenchild_enqueue_parent_and_child_styles() {
 	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 	wp_enqueue_style( 'child-style', get_stylesheet_directory_uri(), array( 'parent-style' ) );
 }
+
+function meks_which_template_is_loaded() {
+	if ( is_super_admin() ) {
+		global $template;
+		print_r( $template );
+	}
+}
+ 
+add_action( 'wp_footer', 'meks_which_template_is_loaded' );
